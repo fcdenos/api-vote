3973b0a38ae260edb65e15f6fed80889f31c571e
Henri Lepic <henrilepic@gmail.com>
2019-11-28 15:23:21 +0100

    add vote function for moke and sqlite

--- db/moke/vote.go
+++ db/moke/vote.go
@@ -6,12 +6,20 @@ import (
 	"github.com/ritoon/api-vote/model"
 )
 
-func (m Moke) Vote(uuid, _ string) error {
-	pi, ok := m.listProposal.Load(uuid)
+func (m Moke) Vote(uuid_user, uuid_proposal string) error {
+	pi, ok := m.listProposal.Load(uuid_proposal)
 	if !ok {
 		return errors.New("proposal not found")
 	}
 	p := pi.(*model.Proposal)
 	p.NBVote = p.NBVote + 1
+
+	ui, ok := m.listUser.Load(uuid_user)
+	if !ok {
+		return errors.New("user not found")
+	}
+	u := ui.(*model.User)
+	u.VoteDone = true
+
 	return nil
 }
--- db/sqlite/vote.go
+++ db/sqlite/vote.go
@@ -1,5 +1,19 @@
 package sqlite
 
-func (s Sqlite) Vote(uuid, _ string) error {
-	return s.db.Exec("UPDATE proposal SET nb_vote = nb_vote + 1 WHERE proposal.uuid = " + uuid).Error
+import "github.com/ritoon/api-vote/model"
+
+func (s Sqlite) Vote(uuid_user, uuid_proposal string) error {
+	user := model.User{
+		ModelDB: model.ModelDB{
+			UUID: uuid_user,
+		},
+	}
+	err := s.db.Model(&user).Updates(model.User{
+		VoteDone: true,
+	}).Error
+	if err != nil {
+		return err
+	}
+
+	return s.db.Exec("UPDATE proposal SET nb_vote = nb_vote + 1 WHERE proposal.uuid = " + uuid_proposal).Error
 }
